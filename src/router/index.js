912c92881ae61f526c2d37de6aaaefd0789291a6
lazyloading <lazyloading@163.com>
2022-09-08 00:07:26 +0800

    modify

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "login",
+    component: () => import("@/views/login/login.vue"),
+    meta: {
+      keepAlive: true,
+      title: "登录",
+    },
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
---
+++ src/views/login/login.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="login">
+    <div class="login_title">登录</div>
+    <div class="input_card">
+      <el-input
+        class="login_input"
+        v-model="account"
+        placeholder="请输入账号"
+      />
+      <el-input
+        class="login_input"
+        v-model="password"
+        type="password"
+        placeholder="请输入密码"
+        show-password
+      />
+    </div>
+
+    <el-button class="login_btn" @click="loginAction">登录</el-button>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, toRefs, Toast } from "vue";
+import { ElMessage } from "element-plus";
+
+export default {
+  name: "login_login",
+
+  setup() {
+    var account = ref("");
+    var password = ref("");
+    const loginAction = () => {
+      if (account.value.length == 0) {
+        console.log(account.value+"233");
+        ElMessage({
+          message: "请输入账号",
+          type: "warning",
+        });
+        return;
+      }
+
+      if (password.value.length == 0) {
+        ElMessage({
+          message: "请输入账号",
+          type: "warning",
+        });
+        return;
+      }
+    };
+
+    return {
+      account,
+      password,
+      loginAction,
+    };
+  },
+};
+</script>
+
+<style>
+.login_title {
+  font-size: 30px;
+  font-weight: 500;
+}
+.input_card {
+  background-color: white;
+  margin: 0px 20%;
+}
+.login_input {
+  margin: 10px 0px;
+}
+.login_btn {
+  margin-top: 50px;
+  width: 60%;
+}
+</style>
